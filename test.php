760c0344a8e603286fe6e5b6ff12a9adf5f305db
gooilipok <gooilipok2@gmail.com>
2026-08-18 14:09:55 +0300

    php upd 9

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode([
+    'status' => 'ok',
+    'file' => 'test.php (root level)',
+    'php_version' => PHP_VERSION,
+    'sapi' => php_sapi_name(),
+    'time' => date('Y-m-d H:i:s'),
+    'document_root' => $_SERVER['DOCUMENT_ROOT'] ?? '',
+    'script' => $_SERVER['SCRIPT_FILENAME'] ?? ''
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
